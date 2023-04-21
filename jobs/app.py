e36e8bfc6087f6fdcf2e30d27e15ef5c20888229
Andrew Price <andrew.price1@landregistry.gov.uk>
2023-04-21 12:20:47 +0100

    Created open_connection function

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+import sqlite3
+
+from flask import Flask, render_template, g
+
+PATH = sqlite3.connect('db/jobs.sqlite')
 
 app = Flask(__name__)
+
+
+def open_connection():
+    getattr(g, '_connection', None)
+    return 'connection'
 
 
 @app.route('/')
