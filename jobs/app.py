e48c21fd4ce67e399e72b0bcf8ce4b6b9c20a38d
Andrew Price <andrew.price1@landregistry.gov.uk>
2023-04-21 12:28:16 +0100

    open_connection completed

--- jobs/app.py
+++ jobs/app.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 
 
 def open_connection():
-    getattr(g, '_connection', None)
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection, g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
     return 'connection'
 
 
