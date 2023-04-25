4ef66d93e106dcd16369a2e0ffacf6db33cb58cc
Andrew Price <andrew.price1@landregistry.gov.uk>
2023-04-25 13:13:18 +0100

    redirect fix

--- jobs/app.py
+++ jobs/app.py
@@ -76,5 +76,5 @@ def review(employer_id):
                     'VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id),
                     commit=True)
-        return redirect(url_for('employer.html', employer_id=employer_id))
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
